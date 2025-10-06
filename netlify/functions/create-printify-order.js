296ef94f2afc896e95159af610e97e02f1dda7f3
Estanler Aleman <Esencymusic@gmail.com>
2025-10-05 23:37:48 -0400

    update

--- netlify/functions/create-printify-order.js
+++ netlify/functions/create-printify-order.js
@@ -1,2 +1,14 @@
 // netlify/functions/create-printify-order.js
-exports.handler = async () => ({ statusCode: 200, body: 'ok' });
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const body = JSON.parse(event.body || '{}');
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ok: true, received: body })
+  };
+};
--- netlify/functions/printify-whoami.js
+++ netlify/functions/printify-whoami.js
@@ -1,12 +1,13 @@
 // netlify/functions/printify-whoami.js
-export async function handler() {
-  const hasKey = !!process.env.PRINTIFY_API_KEY;
+exports.handler = async () => {
+  const hasKey = Boolean(process.env.PRINTIFY_API_KEY);
   return {
     statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       ok: true,
       shopId: process.env.PRINTIFY_SHOP_ID || null,
       hasApiKey: hasKey
     })
   };
-}
+};
